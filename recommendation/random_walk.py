25c11d8948274b82e948d93be803e30467c95583
Themistoklis Spanoudis <themistoklis.spanoudis@gmail.com>
2021-01-07 17:57:46 +0200

    Implemented random_walks function.

--- recommendation/random_walk.py
+++ recommendation/random_walk.py
@@ -2,6 +2,7 @@ import json
 import networkx as nx
 
 from random import choice
+from collections import Counter
 
 
 def build_bipartite_graph(filename):
@@ -63,23 +64,45 @@ def random_neighbor(G, node):
     return randomly_selected_neighbor
 
 
-def random_walk(G, starting_user, iterations):
-    neighbor = random_neighbor(G, starting_user)
-    print("Starting random from user", neighbor)
+def random_walks(G_reduced, target_user, total_steps, double_steps_per_random_walk, verbose=False):
+    seed_repos = list(G_reduced.neighbors(target_user))
+    total_steps_per_repo = round(total_steps / len(seed_repos))
+    random_walks_per_repo = round(total_steps_per_repo / (2 * double_steps_per_random_walk))
+    if random_walks_per_repo == 0:
+        raise(ValueError("Increase total_steps or decrease double_steps_per_random_walk."))
 
-    for i in range(iterations):
-        neighbor = random_neighbor(G, neighbor)
-        if i % 2 == 0:
-            print("Walker is now at user", neighbor)
-        else:
-            print("Walker is now at repo", neighbor)
+    if verbose:
+        print(f"\n\n\nStarting random walks for target user: {target_user}")
+        print(f"The seed repos of the user are: {len(seed_repos)}")
+        print(f"The number of random walks per repo will be: {random_walks_per_repo}")
 
-    return neighbor
+    repo_visit_counts = Counter()
+    for seed_repo in seed_repos:
+        if verbose:
+            print(f"\nStarting random walks for seed repo: {seed_repo}")
+
+        for i in range(random_walks_per_repo):
+            if verbose:
+                print(f"Repo random walk: {i}")
+            repo_node = seed_repo
+
+            for t in range(double_steps_per_random_walk):
+                user_node = random_neighbor(G_reduced, repo_node)
+                repo_node = random_neighbor(G_reduced, user_node)
+                repo_visit_counts.update([repo_node])
+                if verbose:
+                    print(f"{t}: {repo_node}")
+            
+            if verbose:
+                print(f"The repo visit counts are {repo_visit_counts}")
+            
+    return repo_visit_counts
 
 
 if __name__ == "__main__":
     G = build_bipartite_graph("../data/repos_users.json")
 
     target_user = "walsvid"
-    recommended_repo = random_walk(G, target_user, 10)
-    print("For user '{}' is recommended repository: '{}'".format(target_user, recommended_repo))
+    repo_visit_counts = random_walks(G, target_user, total_steps=2000, double_steps_per_random_walk=3)
+    topk = 5
+    print(f"\n\nThe top-5 recommended repos for user {target_user} are: {repo_visit_counts.most_common(topk)}")
