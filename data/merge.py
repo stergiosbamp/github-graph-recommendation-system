0e25102c042f4050e8f6e1c816d755213848a316
stergiosbamp <bampakis.stergios@gmail.com>
2021-01-07 13:04:15 +0200

    Filtering top-3000 users for the final dataset based on their stars

--- data/merge.py
+++ data/merge.py
@@ -6,6 +6,7 @@ from collections import Counter
 
 all_users = Counter()
 repo_users_mapping = dict()
+filtered_users_repos = dict()
 
 path = pathlib.Path("stargazers/")
 
@@ -18,14 +19,27 @@ for repo in path.iterdir():
     with open(repo, "r") as f:
         stargazers = json.load(f)
 
-    repository_name = repo.name.replace("--", "/")
+    repository_name = repo.stem.replace("--", "/")
     # take just username
     users = [user['login'] for user in stargazers]
     repo_users_mapping[repository_name] = users
     all_users.update(users)
 
-print(all_users.most_common(5))
+# Take the top-3000 users with the highest number of stars
+most_common_users = all_users.most_common(3000)
+
+# Get the top users based on their stars
+top_users = []
+for stat in most_common_users:
+    username, stars = stat
+    top_users.append(username)
+
+# Filter out the users based on the top users
+for repo, users in repo_users_mapping.items():
+    filtered_users = set(users).difference(set(top_users))
+    filtered_users_repos[repo] = list(filtered_users)
+
 
 # Write the unified json collection to file
 with open("repos_users.json", "w") as f:
-    json.dump(repo_users_mapping, f, indent=4)
+    json.dump(filtered_users_repos, f, indent=4)
