0f2be6c686e5e3c87b5728cf45fd2f5190affc0a
stergiosbamp <bampakis.stergios@gmail.com>
2021-01-08 11:47:09 +0200

    Implement experiment for various top-k queries

--- recommendation/experiments.py
+++ recommendation/experiments.py
@@ -1,7 +1,8 @@
-from random_walk import *
-
 import random
 import time
+
+from random_walk import *
+
 
 def random_walk_parameters(G):
     repos, users = nx.bipartite.sets(G)
@@ -25,6 +26,29 @@ def random_walk_parameters(G):
             print(f"Recall: @ {topk}: {n_correct/ (portion * len(target_users)):.4f}")
 
     
+def top_k_experiment(G):
+    repos, users = nx.bipartite.sets(G)
+    users = list(users)
+    target_users = random.sample(users, 600)
+
+    portion = 10
+    random_walk_per_repo = 10
+    double_steps_per_random_walk = 4
+
+    top_k_s = [10, 20, 30, 40]
+
+    for topk in top_k_s:
+        n_correct = 0
+        for user in target_users:
+            n_correct += evaluate(G,
+                                  user,
+                                  portion=portion,
+                                  topk=topk,
+                                  random_walks_per_repo=random_walk_per_repo,
+                                  double_steps_per_random_walk=double_steps_per_random_walk)
+        print("\nPerformance for top-k: ", topk)
+        print(f"Precision @ {topk}: {n_correct / (topk * len(target_users)):.4f}")
+        print(f"Recall: @ {topk}: {n_correct / (portion * len(target_users)):.4f}")
 
 
 if __name__ == "__main__":
@@ -32,9 +56,13 @@ if __name__ == "__main__":
 
     G = build_bipartite_graph("../data/repos_users.json")
 
-    random_walk_parameters(G)
+    # random_walk_parameters(G)
+    top_k_experiment(G)
 
 """
+RANDOM WALKS AND DOUBLE STEPS
+-----------------------------
+
 For 3000 users graph size.
 
 Performance for random_walks_per_repo = 5 and double_steps_per_random_walk = 1
@@ -100,4 +128,25 @@ Recall: @ 30: 0.3175
 Performance for random_walks_per_repo = 40 and double_steps_per_random_walk = 4
 Precision @ 30: 0.1078
 Recall: @ 30: 0.3235
+"""
+
+"""
+TOP-K
+------------
+
+Performance for top-k:  10
+Precision @ 10: 0.1183
+Recall: @ 10: 0.1183
+
+Performance for top-k:  20
+Precision @ 20: 0.1037
+Recall: @ 20: 0.2073
+
+Performance for top-k:  30
+Precision @ 30: 0.0977
+Recall: @ 30: 0.2930
+
+Performance for top-k:  40
+Precision @ 40: 0.0932
+Recall: @ 40: 0.3730
 """
